afe27b0d4f215abb1453805fe5d1e9a8bc09b8ac
cityking <154443603@qq.com>
2016-06-12 11:50:34 +0800

    使用SharedPreferences保存数据
    Signed-off-by: cityking <154443603@qq.com>

--- MainActivity.java
+++ MainActivity.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
@@ -19,6 +21,7 @@ public class MainActivity extends Activity {
 	private EditText et_name;
 	private EditText et_pwd;
 	private CheckBox check;
+	private SharedPreferences sharedPreferences;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,46 +30,38 @@ public class MainActivity extends Activity {
 		et_name = (EditText) findViewById(R.id.et_name);
 		et_pwd = (EditText) findViewById(R.id.et_pwd);
 		check = (CheckBox) findViewById(R.id.check);
+		sharedPreferences = getSharedPreferences("user", 0);
 
-		Map<String, String> maps = UserInfoUtils.readInfo(MainActivity.this);
-		if(maps!=null){
-			String name = maps.get("name");
-			String password = maps.get("password");
-			
-			et_name.setText(name);
-			et_pwd.setText(password);
-			
-		}
+		String name = sharedPreferences.getString("name", "");
+		String password = sharedPreferences.getString("pwd", "");
 
-		
+		et_name.setText(name);
+		et_pwd.setText(password);
 
 	}
 
 	public void login(View v) {
 		String userName = et_name.getText().toString().trim();
 		String password = et_pwd.getText().toString().trim();
-		if(TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)){
+		if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)) {
 			Toast.makeText(MainActivity.this, "用户名或密码不能为空", Toast.LENGTH_LONG).show();
-		}else{
+		} else {
 			System.out.println("进入登陆逻辑");
 			if (check.isChecked()) {
-				//判断SD卡是否可用
-				if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-					Toast.makeText(getApplicationContext(), "SD卡可用", 1).show();
-					boolean result = UserInfoUtils.saveInfo(MainActivity.this,userName, password);
-					if(result){
-						Toast.makeText(MainActivity.this, "保存成功", Toast.LENGTH_LONG).show();
-					}else{
-						Toast.makeText(MainActivity.this, "保存失败", Toast.LENGTH_LONG).show();
-					}
-				}else{
-					Toast.makeText(getApplicationContext(), "SD卡不可用", 1).show();
-				}
 				
-			}else{
+				//使用SharedPreferences保存数据
+				SharedPreferences.Editor editor = sharedPreferences.edit();
+				editor.putString("name", userName);
+				editor.putString("pwd", password);
+				editor.commit();
+				
+			} else {
 				Toast.makeText(MainActivity.this, "请勾选保存", Toast.LENGTH_LONG).show();
 			}
 		}
+
+		Intent intent = new Intent(MainActivity.this, GetSDSize.class);
+		startActivity(intent);
 	}
 
 }
